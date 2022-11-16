75dc6a0f7ef0a37988e6017edd6229a18fac135d
Vikram Singh <67967129+vikky413@users.noreply.github.com>
2022-11-16 14:34:01 +0530

    Update setup_k6.sh

--- setup_k6.sh
+++ setup_k6.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 set -ex
-
-sudo apt-get update
-sudo apt-get install dirmngr --install-recommends
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
-echo "deb https://dl.bintray.com/loadimpact/deb stable main" | sudo tee -a /etc/apt/sources.list
-sudo apt-get update
-sudo apt-get install k6
+choco install k6
